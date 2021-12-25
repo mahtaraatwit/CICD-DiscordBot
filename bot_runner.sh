f9e3348dbd5f9f068e5b0057b253f735d12a0f91
amen-ma <mahtara@wit.edu>
2021-12-25 09:05:11 -0500

    legit moved my bot_runner script to yml file. not sure why it couldnt take the .sh i gave it.must look into docs

--- bot_runner.sh
+++ bot_runner.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
 
-cd /Discord
-pipenv shell
-pipenv run python launcher.py
-
-sudo crontab -l > cron_bkp
-*/5 * * * * /Discord/ping.sh >/dev/null 2>&1 >> cron_bkp
-sudo crontab cron_bkp
-sudo rm cron_bkp
