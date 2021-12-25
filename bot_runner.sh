ce585a1fa13e0d2f2bb074f8cfa957377b360076
amen-ma <mahtara@wit.edu>
2021-12-25 09:13:15 -0500

    returned all code backed to bot runner i am fuming rn

--- bot_runner.sh
+++ bot_runner.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 
+sudo crontab -l > cron_bkp
+*/5 * * * * /Discord/ping.sh >/dev/null 2>&1 >> cron_bkp
+sudo crontab cron_bkp
+sudo rm cron_bkp
+
+pipenv shell
+pipenv run python launcher.py
