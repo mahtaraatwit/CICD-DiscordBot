dc4605806264e197e3076d8fc72aff0d18cb2049
amen-ma <mahtara@wit.edu>
2021-12-25 08:57:26 -0500

    added details for cron job and python scripts on bot_runner/ping.sh pings localvpsserverhost/

--- bot_runner.sh
+++ bot_runner.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-touch /Discord
-0 0/5 * 1/1 * ? *
+cd /Discord
+pipenv shell
+pipenv run python launcher.py
+
+sudo crontab -l > cron_bkp
+*/5 * * * * /Discord/ping.sh >/dev/null 2>&1 >> cron_bkp
+sudo crontab cron_bkp
+sudo rm cron_bkp
--- ping.sh
+++ ping.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-HOSTS=""
+HOSTS="http://138.197.9.17:8080/"
 COUNT=4
 
 ping -c $COUNT $HOSTS
