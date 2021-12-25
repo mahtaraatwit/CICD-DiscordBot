c2774dd8e5f1b1358084bc50802d350e8144f004
amen-ma <mahtara@wit.edu>
2021-12-25 09:33:55 -0500

    does exit code 0 still let it run??

--- bot_runner.sh
+++ bot_runner.sh
@@ -5,6 +5,8 @@ sudo echo "*/5 * * * * /Discord/ping.sh >/dev/null 2>&1" >> cron_bkp
 sudo crontab cron_bkp
 sudo rm cron_bkp
 
+kill -9 $(ps -A | grep python | awk '{print $1}')
+
 pipenv shell
 pipenv run python launcher.py
 exit 0
